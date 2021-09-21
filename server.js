13c4526e8a42f1f20d4c94f912f32962c0515f50
Shawn Su <shawnsu@developersinstitute.co.nz>
2021-09-21 21:32:39 +1200

    add: POST /api/shorturl, GET /api/shorturl/:surl

--- server.js
+++ server.js
@@ -1,24 +1,51 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-
+const bodyParser = require('body-parser')
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
+// Storing url/shorturl pairs
+const urlLookup = {
+  1: "https://www.google.co.nz",
+  2: "https://www.apple.co.nz",
+  3: "https://www.newzealand.com",
+};
+
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) 
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// endpoint for creating shorturl
+app.post("/api/shorturl", function (req, res) {
+  // { original_url : 'https://freeCodeCamp.org', short_url : 1}
+  const original_url = req.body.url
+  const short_url = Object.keys(urlLookup).length + 1
+  urlLookup[short_url] = original_url
+  console.log(req.body)
+  res.json({original_url, short_url});
+});
+
+// endpoint for retrieving fullurl
+app.get("/api/shorturl/:surl", function (req, res) {
+  const short_url = req.params.surl
+  const original_url = urlLookup[short_url]
+  res.redirect(301, original_url);
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
